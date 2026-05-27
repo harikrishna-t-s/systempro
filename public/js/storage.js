a883564cf039617ca4701874bf6b63a92f3b71a2
Harikrishna <hk9117430@gmail.com>
2026-05-27 21:14:06 +0530

    chores: happyyyy

---
+++ public/js/storage.js
@@ -0,0 +1,61 @@
+class SystemProStorage {
+    constructor() {
+        this.dbName = 'SystemProDB';
+        this.version = 1;
+        this.db = null;
+    }
+
+    init() {
+        return new Promise((resolve, reject) => {
+            const request = indexedDB.open(this.dbName, this.version);
+
+            request.onerror = () => reject(request.error);
+            request.onsuccess = () => {
+                this.db = request.result;
+                resolve();
+            };
+
+            request.onupgradeneeded = (event) => {
+                const db = event.target.result;
+                if (!db.objectStoreNames.contains('entries')) {
+                    db.createObjectStore('entries', { keyPath: 'id' });
+                }
+            };
+        });
+    }
+
+    getAllEntries() {
+        return new Promise((resolve, reject) => {
+            const transaction = this.db.transaction(['entries'], 'readonly');
+            const store = transaction.objectStore(['entries']);
+            const request = store.getAll();
+
+            request.onsuccess = () => resolve(request.result);
+            request.onerror = () => reject(request.error);
+        });
+    }
+
+    saveEntry(entry) {
+        return new Promise((resolve, reject) => {
+            const transaction = this.db.transaction(['entries'], 'readwrite');
+            const store = transaction.objectStore(['entries']);
+            const request = store.put(entry);
+
+            request.onsuccess = () => resolve();
+            request.onerror = () => reject(request.error);
+        });
+    }
+
+    deleteEntry(id) {
+        return new Promise((resolve, reject) => {
+            const transaction = this.db.transaction(['entries'], 'readwrite');
+            const store = transaction.objectStore(['entries']);
+            const request = store.delete(id);
+
+            request.onsuccess = () => resolve();
+            request.onerror = () => reject(request.error);
+        });
+    }
+}
+
+window.storageEngine = new SystemProStorage();
